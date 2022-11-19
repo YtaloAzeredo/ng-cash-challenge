a9ded1a28414f83929d08368c64f00fde3f4d6b9
ytalo azeredo <ytaloazeredo@gmail.com>
2022-11-19 20:12:41 -0300

    feat: post users validator

--- src/shared/infra/http/routes/validators/users-routes.validator.ts
+++ src/shared/infra/http/routes/validators/users-routes.validator.ts
@@ -2,6 +2,15 @@ import { body } from 'express-validator'
 import { defaultCheck } from './default.validator'
 
 export const postUsersValidator = [
-  body('username').isString().isLength({ min: 3 }).withMessage('Username must be at least 3 characters long'),
+  body('username')
+    .isString()
+    .isLength({ min: 3 })
+    .withMessage('Username must be at least 3 characters long'),
+  body('password')
+    .isString()
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters long')
+    .matches(/[0-9]+[A-Z]+/)
+    .withMessage('Password must contain at least one number and one capital letter'),
   defaultCheck
 ]
