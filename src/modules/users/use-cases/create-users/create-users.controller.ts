4fb5b0927cff776defa63145d4969e4d80939b2e
ytalo azeredo <ytaloazeredo@gmail.com>
2022-11-19 11:39:59 -0300

    feat: tagging injectable users controller

--- src/modules/users/use-cases/create-users/create-users.controller.ts
+++ src/modules/users/use-cases/create-users/create-users.controller.ts
@@ -1,8 +1,9 @@
 import { UseCase } from '@shared/presentation/protocols/use-case.interface'
-import { inject } from 'tsyringe'
+import { inject, injectable } from 'tsyringe'
 import { Controller } from '@presentation/protocols/controller.interface'
 import { HttpRequest, HttpResponse } from '@shared/presentation/protocols/http.interface'
 
+@injectable()
 export class CreateUsersController implements Controller {
   constructor (
     @inject('CreateUsersUseCase')
