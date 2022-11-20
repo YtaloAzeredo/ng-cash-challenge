dc10b51dfa9304bd8956dc79bb38173be4cb4c49
ytalo azeredo <ytaloazeredo@gmail.com>
2022-11-19 23:19:03 -0300

    feat: enter and withdraw balance routes

--- src/shared/infra/http/routes/accounts.routes.ts
+++ src/shared/infra/http/routes/accounts.routes.ts
@@ -1,5 +1,7 @@
 import { adaptRoute } from '@adapters/express-route.adapter'
+import { EnterBalanceController } from '@modules/accounts/use-cases/enter-balance/enter-balance.controller'
 import { GetOneAccountsController } from '@modules/accounts/use-cases/get-one-accounts/get-one-accounts.controller'
+import { WithdrawBalanceController } from '@modules/accounts/use-cases/withdraw-balance/withdraw-balance.controller'
 import { Router } from 'express'
 
 const router = Router()
@@ -7,6 +9,16 @@ const router = Router()
 router.get(
   '/accounts/users/:userId',
   adaptRoute(GetOneAccountsController)
+)
+
+router.post(
+  '/accounts/:accountId/enter-balance',
+  adaptRoute(EnterBalanceController)
+)
+
+router.post(
+  '/accounts/:accountId/withdraw-balance',
+  adaptRoute(WithdrawBalanceController)
 )
 
 export default router
