ccf58dadcda7daea521b05a4e70e0795b373bdf4
ytalo azeredo <ytaloazeredo@gmail.com>
2022-11-20 16:09:40 -0300

    feat: container register decode token use case

--- src/shared/container/providers/jwt/index.ts
+++ src/shared/container/providers/jwt/index.ts
@@ -1,8 +1,15 @@
 import { container } from 'tsyringe'
 import { CreateToken } from './interfaces/create-token.interface'
+import { DecodeToken } from './interfaces/decode-token.interface'
 import { JwtCreateTokenUseCase } from './use-cases/create-token.use-case'
+import { JwtDecodeTokenUseCase } from './use-cases/decode-token.use-case'
 
 container.registerSingleton<CreateToken>(
   'CreateTokenUseCase',
   JwtCreateTokenUseCase
+)
+
+container.registerSingleton<DecodeToken>(
+  'DecodeTokenUseCase',
+  JwtDecodeTokenUseCase
 )
