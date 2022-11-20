f183ac7321da6ce6990f4f95473ab658969b9986
ytalo azeredo <ytaloazeredo@gmail.com>
2022-11-20 00:20:39 -0300

    feat: register transactions

--- src/modules/accounts/use-cases/enter-balance/enter-balance.use-case.ts
+++ src/modules/accounts/use-cases/enter-balance/enter-balance.use-case.ts
@@ -1,6 +1,8 @@
 import NotFoundError from '@errors/not-found.error'
 import { AccountsModel } from '@modules/accounts/models/accounts-model.interface'
 import { AccountsRepository } from '@modules/accounts/repositories/accounts-repository.interface'
+import { TransactionCodes } from '@modules/transactions/constants/transaction-codes.enum'
+import { TransactionsRepository } from '@modules/transactions/repositories/transactions-repository.interface'
 import { UseCase } from '@shared/presentation/protocols/use-case.interface'
 import { inject, injectable } from 'tsyringe'
 
@@ -8,13 +10,20 @@ import { inject, injectable } from 'tsyringe'
 export class EnterBalanceUseCase implements UseCase {
   constructor (
     @inject('AccountsRepository')
-    private readonly accountsRepository: AccountsRepository
+    private readonly accountsRepository: AccountsRepository,
+    @inject('TransactionsRepository')
+    private readonly transactionRepository: TransactionsRepository
   ) {}
 
   async execute (accountData: AccountsModel): Promise<AccountsModel> {
     const foundAccount = await this.accountsRepository.getOne({ id: accountData.id })
     if (!foundAccount) throw new NotFoundError('Account not found')
     foundAccount.balance += accountData.balance
+    await this.transactionRepository.store({
+      creditedAccount: foundAccount.id,
+      value: accountData.balance,
+      code: TransactionCodes['cash-in']
+    })
     return this.accountsRepository.save(foundAccount)
   }
 }
--- src/modules/accounts/use-cases/withdraw-balance/withdraw-balance.use-case.ts
+++ src/modules/accounts/use-cases/withdraw-balance/withdraw-balance.use-case.ts
@@ -2,6 +2,8 @@ import ConflictError from '@errors/conflict.error'
 import NotFoundError from '@errors/not-found.error'
 import { AccountsModel } from '@modules/accounts/models/accounts-model.interface'
 import { AccountsRepository } from '@modules/accounts/repositories/accounts-repository.interface'
+import { TransactionCodes } from '@modules/transactions/constants/transaction-codes.enum'
+import { TransactionsRepository } from '@modules/transactions/repositories/transactions-repository.interface'
 import { UseCase } from '@shared/presentation/protocols/use-case.interface'
 import { inject, injectable } from 'tsyringe'
 
@@ -9,7 +11,9 @@ import { inject, injectable } from 'tsyringe'
 export class WithdrawBalanceUseCase implements UseCase {
   constructor (
     @inject('AccountsRepository')
-    private readonly accountsRepository: AccountsRepository
+    private readonly accountsRepository: AccountsRepository,
+    @inject('TransactionsRepository')
+    private readonly transactionRepository: TransactionsRepository
   ) {}
 
   async execute (accountData: AccountsModel): Promise<AccountsModel> {
@@ -18,6 +22,11 @@ export class WithdrawBalanceUseCase implements UseCase {
     const canWithdrawBalance = foundAccount.balance >= accountData.balance
     if (!canWithdrawBalance) throw new ConflictError('Insufficient balance for withdrawal')
     foundAccount.balance -= accountData.balance
+    await this.transactionRepository.store({
+      debitedAccount: foundAccount.id,
+      value: accountData.balance,
+      code: TransactionCodes['cash-out']
+    })
     return this.accountsRepository.save(foundAccount)
   }
 }
